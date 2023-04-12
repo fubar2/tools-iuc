508d68d6a2fc6af23c6740be69ab944329a44216
paulzierep <paul.zierep@googlemail.com>
2023-04-12 11:54:44 +0200

    init

--- data_managers/data_manager_pharokka_database_fetcher/data_manager/data_manager_fetch_pharokka_db.py
+++ data_managers/data_manager_pharokka_database_fetcher/data_manager/data_manager_fetch_pharokka_db.py
@@ -1,23 +1,18 @@
 #!/usr/bin/env python
 
 import argparse
-import json
+import uuid
 import os
+from datetime import datetime
+import json
 import subprocess
 import sys
-from datetime import datetime
-
 
 def main():
-    # Parse Command Line
-    parser = argparse.ArgumentParser(description="Create data manager JSON.")
-    parser.add_argument("--out", dest="output", action="store", help="JSON filename")
-    parser.add_argument("--version", dest="version", action="store", help="Version of the DB")
-    parser.add_argument(
-        "--test",
-        action="store_true",
-        help="option to test the script with an lighted database",
-    )
+    #Parse Command Line
+    parser = argparse.ArgumentParser(description='Create data manager JSON.')
+    parser.add_argument('--out', dest='output', action='store', help='JSON filename')
+    parser.add_argument("--test", action='store_true', help="option to test the script with an lighted database")
 
     args = parser.parse_args()
 
@@ -27,30 +22,21 @@ def main():
     with open(args.output) as fh:
         params = json.load(fh)
 
-    workdir = params["output_data"][0]["extra_files_path"]
+
+    workdir = params['output_data'][0]['extra_files_path']
     os.mkdir(workdir)
 
     time = datetime.utcnow().strftime("%Y-%m-%dT%H%M%SZ")
     db_value = "db_from_{0}".format(time)
     db_path = os.path.join(workdir, db_value)
 
-    # create DB
-    if args.test:  # the test only checks that the pharokka download script is available and copies the test DB
-
-        # check if install_databases.py is there
-        command_args = ["install_databases.py", "-h"]
-        proc = subprocess.Popen(args=command_args, shell=False)
-        return_code = proc.wait()
-        if return_code:
-            print("Error downloading Pharokka database.", file=sys.stderr)
-            sys.exit(return_code)
-
-        # copy the test DB
-        test_db_path = os.path.join(os.path.dirname(os.path.realpath(__file__)), "subset_pharokka_db")
-        command_args = ["cp", "-r", test_db_path, db_path]
+    #create DB
+    if args.test: #the test just checks that the pharokka download script is available
+        command_args = ["install_databases.py", "-h"]   
     else:
         command_args = ["install_databases.py", "-o", db_path]
 
+    # command_args = ["mkdir", db_path]
     proc = subprocess.Popen(args=command_args, shell=False)
     return_code = proc.wait()
     if return_code:
@@ -59,20 +45,18 @@ def main():
 
     # Update Data Manager JSON and write to file
     data_manager_entry = {
-        "data_tables": {
-            "pharokka_db": {
-                "value": db_value,
-                "dbkey": db_value,
-                "version": args.version,
-                "name": f"Pharokka DB version {args.version} downloaded at {datetime.now()}",
-                "path": db_path,
+        'data_tables': {
+            'pharokka_db': {
+                'value': db_value,
+                'dbkey': db_value,
+                'name': 'Pharokka DB downloaded at {0}'.format(datetime.now()),
+                'path': db_path
             }
         }
     }
 
-    with open(os.path.join(args.output), "w+") as fh:
+    with open(os.path.join(args.output), 'w+') as fh:
         json.dump(data_manager_entry, fh, sort_keys=True)
 
-
-if __name__ == "__main__":
+if __name__ == '__main__':
     main()
